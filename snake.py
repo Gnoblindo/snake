ba88d9b63d023ca65835c3702e1d8edb893371bc
Gnoblindo <szymon.przybyla2010@gmail.com>
2025-03-08 16:49:04 +0100

    grid

---
+++ snake.py
@@ -0,0 +1,42 @@
+import math;
+import random;
+import pygame;
+import tkinter as tk;
+from tkinter import messagebox;
+
+
+def draw_grid(w, rows, surface):
+    size_between = w // rows
+
+    x = 0
+    y = 0
+    for i in range(rows):
+        x = x + size_between
+        y = y + size_between
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
+
+def draw_window(surface):
+    surface.fill((100, 0, 100))
+    draw_grid(size, rows, surface)
+    pygame.display.update()
+
+def main():
+    global size, rows
+    size = 500
+    rows = 20
+    window = pygame.display.set_mode((size, size))
+
+    flag = True
+    clock = pygame.time.Clock()
+
+    while flag:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                flag = False
+        pygame.time.delay(50)
+        clock.tick(10)
+
+        draw_window(window)
+
+main()
